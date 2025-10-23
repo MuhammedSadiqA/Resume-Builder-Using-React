7a2af8d08cc6c1c0495fc3b462baf9829a217ef2
MuhammedSadiqA <muhammedsadiq3669@gmail.com>
2025-10-23 10:27:55 +0530

    resume-builder-created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+ 
+  return (
+    <>
+<div className='btn d-flex align-items-center'>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+</div>
+    </>
+  )
+}
+export default App
